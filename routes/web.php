7a472633916aca5d6a9db51de9c662676abd9367
CandraApriditaPutri <candra.apridita@gmail.com>
2021-09-07 12:25:53 +0700

    Praktikum 1

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    echo('Selamat Datang');
+});
+
+
+Route::get('/about', function () {
+    echo('NIM : 2031710048<BR> NAMA : CANDRA APRIDITA PUTRI');
+});
+
+
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman artikel dengan ID '.$id;
+});
